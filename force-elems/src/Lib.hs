c900c7030b7633566fcccf586943c8658206802c
Martin A.T. Handley <martinhandley91@gmail.com>
2021-01-19 22:01:35 +0000

    Update Lib.hs

--- force-elems/src/Lib.hs
+++ force-elems/src/Lib.hs
@@ -22,3 +22,20 @@ forceElems :: Traversable t => t a -> t a
 forceElems  
   = unBox 
   . traverse (\x -> x `seq` Box x)
+   
+-- newtype Codensity a = Codensity { runCodensity :: forall r . (a -> r) -> r }
+--
+-- instance Functor Codensity where
+--  -- Too strict for test 8, but rest fine
+--  -- fmap f (Codensity k1) = Codensity (\k -> k1 (k . f))     
+--  fmap f cod = pure f <*> cod
+
+-- instance Applicative Codensity where
+--  pure x = Codensity (\k -> k x)
+
+--  Codensity kf <*> Codensity kx = Codensity (\k -> k (kx (kf id)))
+--  -- Too strict in general?
+--  -- Codensity kf <*> Codensity kx = Codensity (\k -> kf (\f -> kx (\a -> k (f a)))) 
+
+-- forceElems :: Traversable t => t a -> t a
+-- forceElems  = (`runCodensity` id) . traverse (\x -> x `seq` pure x)
