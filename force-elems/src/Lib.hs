827021fd7a3f5215fa27c9cdc0bbd733db4098fb
Martin A.T. Handley <martinhandley91@gmail.com>
2021-01-19 21:47:41 +0000

    Update Lib.hs
    Solve `forceElems`

--- force-elems/src/Lib.hs
+++ force-elems/src/Lib.hs
@@ -1,6 +1,24 @@
 module Lib
-    ( forceElems
-    ) where
+  ( forceElems
+  ) 
+where
+
+-- Doesn't work with newtype
+data Box a = Box a
+
+unBox :: Box a -> a
+unBox (Box x) = x
+
+instance Functor Box where
+  fmap f box = pure f <*> box
+
+instance Applicative Box where
+  pure = Box
+  
+  -- Doesn't work without the case
+  fBox <*> xBox = Box (case xBox of { Box x -> unBox fBox x })
 
 forceElems :: Traversable t => t a -> t a
-forceElems = undefined
+forceElems  
+  = unBox 
+  . traverse (\x -> x `seq` Box x)
